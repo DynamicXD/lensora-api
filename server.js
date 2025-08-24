b8f2eeca972d79d52eeea2e43b26c1192bcb41db
Dynamic <dynamicfighter2357@gmail.com>
2025-08-24 21:53:36 +0530

    sdfs

--- server.js
+++ server.js
@@ -48,7 +48,6 @@ app.use(limiter);
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
